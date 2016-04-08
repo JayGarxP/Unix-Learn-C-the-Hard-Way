6b0daca8ae29316ce52249cb716780517bb14c84
joseph.fallon <joseph.fallon@brynhildr-new.students.cset.oit.edu>
2016-04-07 17:24:49 -0700

    Initial commit

---
+++ EX3/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(int argc, char*argv[])
+{
+    int dist = 100;  
+
+    printf("I am %d years old. \n", dist);
+
+    //Comment ALOT; at least it helps valgrind
+   /*Mulit
+    * line
+    * comment
+    */
+
+
+
+    return 0;
+}
