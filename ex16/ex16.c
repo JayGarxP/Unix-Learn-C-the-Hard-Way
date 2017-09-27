5ab37a93760c876d277a9e146430045b98386c2f
JayGarxP <jpfallon3@gmail.com>
2017-09-26 19:13:30 -0700

    C95 struct syntax differences illustrated

--- ex16/ex16.c
+++ ex16/ex16.c
@@ -5,26 +5,27 @@
 #include "str2int.h"
 //CygWin contains does NOT support all of POSIX, it actually sux. Can't check for memory leaks easily or fork() reliably. Going to have to switch to new environment. :(
 //typing this within ubuntu terminal on different machine from year 2010
-struct Person {
-	char*name;
-	int age;
-	int height;
-	int weight;
-};
-
-//On certain machines and worse versions of C you will have to use gory syntax to avoid namespace issues;
-//for example, if you were using this struct in another file (forward declaration)
-//typedef struct UnimportantPerson {
+//struct Person {
 //	char*name;
 //	int age;
 //	int height;
 //	int weight;
-//}Person;
+//};
 
-struct Person* Person_create(char*name, int age, int height, int weight)
+//On certain machines and worse versions of C you will have to use gory syntax to avoid namespace issues;
+//for example, if you were using this struct in another file (forward declaration)
+typedef struct UnimportantPerson {
+	char*name;
+	int age;
+	int height;
+	int weight;
+}Person;
+
+//struct Person* Person_create(char*name, int age, int height, int weight)
+Person * Person_create(char*name, int age, int height, int weight)
 {
-	struct Person* newPerson = malloc(sizeof(struct Person));
-	//Person * newPerson = malloc(sizeof(Person));
+	//struct Person* newPerson = malloc(sizeof(struct Person));
+	Person * newPerson = malloc(sizeof(Person));
 	assert(newPerson != NULL); //malloc will point to NULL if malloc couldn't allocate any memory for ya
 
 	newPerson->name = strdup(name);
@@ -36,7 +37,8 @@ struct Person* Person_create(char*name, int age, int height, int weight)
 }
 
 //VALGRIND is not supported in CygWin :( going to try 'dmalloc' custom tool; dmalloc isn't working either, I'm either missing packages or it doesn't work on x64 CygWin :(
-void Person_destroy(struct Person * whomst)
+//void Person_destroy(struct Person * whomst)
+void Person_destroy(Person * whomst)
 {
 	assert(whomst != NULL);
 	free(whomst->name);
@@ -44,7 +46,8 @@ void Person_destroy(struct Person * whomst)
 }
 
 //If only I'd thought of the right words, I could have held onto your heart
-void Person_print(struct Person *who)
+//void Person_print(struct Person *who)
+void Person_print(Person* who)
 {
 	printf("Name: %s\n", who->name);
 	printf("\tAge: %d\n", who->age);
@@ -55,8 +58,10 @@ void Person_print(struct Person *who)
 
 int main(int argc, char * argv[])
 {
-	struct Person * Qui = Person_create("BonQuiQui", 32, 64, 140);
-	struct Person * SarahBarthel = Person_create("Sarah", 20, 72, 180);
+	//struct Person * Qui = Person_create("BonQuiQui", 32, 64, 140);
+	Person * Qui = Person_create("BonQuiQui", 32, 64, 140);
+	//struct Person * SarahBarthel = Person_create("Sarah", 20, 72, 180);
+	Person * SarahBarthel = Person_create("Sarah", 20, 72, 180);
 
 	printf("Qui is at memory location %p: \n", Qui);
 	Person_print(Qui);
